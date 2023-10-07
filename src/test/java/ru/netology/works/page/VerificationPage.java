5a676f806098c1944055f40508605f068bc86245
Vladimir444k <gluschenkovladimir337@gmail.com>
2023-10-07 17:53:50 +0300

    mysql comm

---
+++ src/test/java/ru/netology/works/page/VerificationPage.java
@@ -0,0 +1,30 @@
+package ru.netology.works.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private final SelenideElement errorNotification = $("[data-test-id='error-notification']");
+
+    public void verifyVerificationPageVis() {
+        codeField.shouldBe(visible);
+    }
+
+    public void verifyErrorNotificationVis() {
+        errorNotification.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(String verificationCode) {
+        verify(verificationCode);
+        return new DashboardPage();
+    }
+
+    public void verify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+}
---
+++ src/test/java/ru/netology/works/test/BankLoginTest.java
@@ -0,0 +1,54 @@
+package ru.netology.works.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.works.data.DataHelper;
+import ru.netology.works.data.SQLHelper;
+import ru.netology.works.page.DashboardPage;
+import ru.netology.works.page.LoginPage;
+
+
+import javax.xml.crypto.Data;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.works.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exits login and password from sut test data")
+    void testSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999",LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVis();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void testGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisiblity();
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with exist in base and active user and random verification code")
+    void testGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVis();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVis();
+    }
+}
